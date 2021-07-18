d2c6b382e1f41dbe9c5d819a91d8d4bd6ca3ca39
johnshawnwhite <82325558+johnshawnwhite@users.noreply.github.com>
2021-07-17 21:50:59 -0400

    reducers in place in index

--- src/index.js
+++ src/index.js
@@ -15,15 +15,44 @@ import { Provider } from 'react-redux';
 function feedbackReducer( state ={}, action) {
     return state;
 }
+
+const feelingReducer = ( state =[], action) => {
+    if (action.type === "ADD_NEW_FEELING") {
+        console.log(`The feeling added is ${action.payload}`);
+        return action.payload;
+    }
+    return state;
+}
+const understandingReducer = ( state =[], action) => {
+    if (action.type === "ADD_NEW_UNDERSTANDING") {
+        console.log(`The feeling added is ${action.payload}`);
+        return action.payload;
+    }
+    return state;
+}
+const supportReducer = ( state =[], action) => {
+    if (action.type === "ADD_NEW_SUPPORT") {
+        console.log(`The feeling added is ${action.payload}`);
+        return action.payload;
+    }
+    return state;
+}
+const commentsReducer = ( state =[], action) => {
+    if (action.type === "ADD_NEW_COMMENT") {
+        console.log(`The feeling added is ${action.payload}`);
+        return action.payload;
+    }
+    return state;
+}
 // storeInstance goes here
 //reducers also go in here
 const storeInstance = createStore(
     combineReducers({
-        feedbackReducer
-        //feeling
-        //understanding
-        //support
-        //comments
+        feedbackReducer,
+        feelingReducer,
+        understandingReducer,
+        supportReducer,
+        commentsReducer
     }),
     //apply logger here
     applyMiddleware(logger),
